175dc760d9388e414c87b3abc88be851767c2587
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2023-11-01 22:10:35 +0100

    👷 Adding Vercel adapter for prod and preview

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 import { defineConfig } from "astro/config";
-
+import vercelStatic from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
+
+const confProd = {
+  output: "static",
+  adapter: vercelStatic(),
+};
+
+const confPreview = {
+  output: "server",
+  adapter: vercel(),
+};
 
 // https://astro.build/config
 export default defineConfig({
+  ...(process.env.NODE_ENV === "production" ? confProd : confPreview),
   integrations: [tailwind()],
-  ssr: !import.meta.env.PROD,
 });
