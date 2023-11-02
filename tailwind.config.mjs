4cc641fc49571085193354ae1ada9c5ef63c5663
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2023-11-02 17:33:37 +0100

    📱 Fixing responsive design for home section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,14 +5,7 @@ export default {
     fontFamily: {
       sans: ["Poiret One", "sans-serif"],
     },
-    extend: {
-      gridTemplateColumns: {
-        "fit-25": "repeat(auto-fit, minmax(25rem, 1fr))",
-      },
-      boxShadow: {
-        all: "0 .15rem .5rem rgba(3,8,20,.1),0 .075rem .175rem rgba(2,8,20,.1);",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
